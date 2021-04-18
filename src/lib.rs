f344561b372563aa8d065e80d9baf506ce556df2
Daniel Franklin <daniel@danielzfranklin.org>
2021-04-18 16:31:42 +0100

    Move alternatives to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -101,6 +101,20 @@
 //!     }
 //! }
 //! ```
+//!
+//! # Alternatives
+//!
+//! - [minimal-fixtures][minimal-fixtures]
+//! - [rstest][rstest]
+//! - [test-env-log][test-env-log]
+//!
+//! I want to especially thank d-e-s-o for test-env-log. If I hadn't seen it, using
+//! macros to reduce redundant test setup wouldn't have occurred to me.
+//!
+//! [minimal-fixtures]: https://github.com/vorner/minimal-fixtures
+//! [rstest]: https://github.com/la10736/rstest
+//! [test-env-log]: https://github.com/d-e-s-o/test-env-log
+//! [crates-io]: https://crates.io/crates/wraptest
 
 #![warn(clippy::cargo)]
 
